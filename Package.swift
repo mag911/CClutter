63523d0267f90eb99bb0a81cbb73d621ad8bf4ef
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-13 15:35:53 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,3 +1,9 @@
 import PackageDescription 
 
-let package = Package(name: "CClutter")
+let package = Package(name: "CClutter",
+	pkgConfig: "clutter-1.0 cogl-gl-1.0 cogl-path-1.0 cairo glib-2.0 gio-unix-2.0",
+	providers: [
+		.brew(["clutter", "cogl", "cairo", "glib", "glib-networking", "gobject-introspection"]),
+		.apt(["libclutter1.0-dev", "libcogl1.0-dev", "libcairo2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
+	]
+)
